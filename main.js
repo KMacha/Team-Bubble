138626d8114de957e4abafdfae75874743e2fc50
Tega kofi <kofitega@gmail.com>
2019-09-21 01:46:01 +0200

    deleted the form esther created because it interfered with the website

--- main.js
+++
@@ -1,7 +0,0 @@
-var message = document.querySelector('message a');
-var form = document.querySelector('form');
-
-
-        $('.message a').click(function(){
-         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-      });
